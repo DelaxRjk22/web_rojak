b404043e180dea1438cbd823a6c734094e08dcd7
DelaxRjk22 <muhammadkurniarazak@gmail.com>
2023-06-20 13:07:21 +0700

    [web_rojak]

---
+++ Website_saya/buku.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Website Saya</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Selamat Datang Di halaman saya</h1>
+    <hr>
+    <ul>
+        <a href="buku.php">
+            <li>Home</li>
+        </a>
+        <a href="index.php">
+            <li>Pinjam Buku</li>
+        </a>
+    </ul>
+    <hr>
+    <a href="tambah_buku.php">Tambah Buku</a>
+    <table>
+        </tr>
+             <th>ID</th>
+             <th>Judul Buku</th>
+             <th>Tahun Terbit</th>
+             <th>Penulis</th>
+             <th>Penerbit</th>
+             <th>Update</th>
+             <th>Opsi</th>
+        </tr>
+    </table>
+</body>
+</html>
---
+++ Website_saya/tambah_buku.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Website saya</title>
+
+    <link rel="stylessheet" href="style.css">
+
+
+</head>
+<body>
+    <h1>Selamat Datang di Halaman Tambah Buku</h1>
+    <hr>
+    <ul>
+        <a href="buku.php">
+            <li>Halaman Buku</li>
+        </a>
+        <a href="pinjam.php">
+            <li>Pinjam Buku</li>
+</body>
+</html>
